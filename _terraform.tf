2febd6c66e8b255d46fce014d139c062b63f3332
Ondrej Sika <ondrej@ondrejsika.com>
2025-04-08 22:08:59 +0200

    feat: Merge versions.tf and providers.tf to _terraform.tf

--- _terraform.tf
+++ _terraform.tf
@@ -9,3 +9,13 @@ terraform {
   }
   required_version = ">= 0.13"
 }
+
+variable "digitalocean_token" {}
+provider "digitalocean" {
+  token = var.digitalocean_token
+}
+
+variable "cloudflare_api_token" {}
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
